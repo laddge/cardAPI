4654d4c535feb376d3aa01c657625f2ea4609862
Laddge <dev.laddge@gmail.com>
2021-06-02 22:44:20 +0900

    [feat] add twitter card support

--- api/__init__.py
+++ api/__init__.py
@@ -9,26 +9,32 @@ def main(url):
     except Exception as e:
         print(e)
         return {}
-    title = (
-        html.xpath('.//meta[@property="og:title"]/@content')[-1]
-        if html.xpath('.//meta[@property="og:title"]/@content')
-        else ''
-    )
-    description = (
-        html.xpath('.//meta[@property="og:description"]/@content')[-1]
-        if html.xpath('.//meta[@property="og:description"]/@content')
-        else ''
-    )
-    site_name = (
-        html.xpath('.//meta[@property="og:site_name"]/@content')[-1]
-        if html.xpath('.//meta[@property="og:site_name"]/@content')
-        else ''
-    )
-    image = (
-        html.xpath('.//meta[@property="og:image"]/@content')[-1]
-        if html.xpath('.//meta[@property="og:image"]/@content')
-        else ''
-    )
+    if html.xpath('.//meta[@property="og:title"]/@content'):
+        title = html.xpath('.//meta[@property="og:image"]/@content')[-1]
+    elif html.xpath('.//meta[@property="twitter:title"]/@content'):
+        title = html.xpath('.//meta[@property="twitter:title"]/@content')[-1]
+    else:
+        title = ''
+
+    if html.xpath('.//meta[@property="og:description"]/@content'):
+        description = html.xpath('.//meta[@property="og:image"]/@content')[-1]
+    elif html.xpath('.//meta[@property="twitter:description"]/@content'):
+        description = html.xpath('.//meta[@property="twitter:description"]/@content')[-1]
+    else:
+        description = ''
+
+    if html.xpath('.//meta[@property="og:site_name"]/@content'):
+        site_name = html.xpath('.//meta[@property="og:image"]/@content')[-1]
+    else:
+        site_name = ''
+
+    if html.xpath('.//meta[@property="og:image"]/@content'):
+        image = html.xpath('.//meta[@property="og:image"]/@content')[-1]
+    elif html.xpath('.//meta[@property="twitter:image"]/@content'):
+        image = html.xpath('.//meta[@property="twitter:image"]/@content')[-1]
+    else:
+        image = ''
+
     return {
         "title": title,
         "description": description,
