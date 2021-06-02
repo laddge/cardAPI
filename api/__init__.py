22cde0cc6001ba741ee5ebc9b3bb3a95938a57f5
Laddge <dev.laddge@gmail.com>
2021-06-02 22:30:49 +0900

    [fix] add exceptions

--- api/__init__.py
+++ api/__init__.py
@@ -3,15 +3,35 @@ import lxml.html
 
 
 def main(url):
-    req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    html = lxml.html.fromstring(request.urlopen(req).read())
-    title = html.xpath('.//meta[@property="og:title"]/@content')[0]
-    description = html.xpath('.//meta[@property="og:description"]/@content')[0]
-    site_name = html.xpath('.//meta[@property="og:site_name"]/@content')[0]
-    image = html.xpath('.//meta[@property="og:image"]/@content')[0]
+    req = request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    try:
+        html = lxml.html.fromstring(request.urlopen(req).read())
+    except Exception as e:
+        print(e)
+        return {}
+    title = (
+        html.xpath('.//meta[@property="og:title"]/@content')[0]
+        if html.xpath('.//meta[@property="og:title"]/@content')
+        else ''
+    )
+    description = (
+        html.xpath('.//meta[@property="og:description"]/@content')[0]
+        if html.xpath('.//meta[@property="og:description"]/@content')
+        else ''
+    )
+    site_name = (
+        html.xpath('.//meta[@property="og:site_name"]/@content')[0]
+        if html.xpath('.//meta[@property="og:site_name"]/@content')
+        else ''
+    )
+    image = (
+        html.xpath('.//meta[@property="og:image"]/@content')[0]
+        if html.xpath('.//meta[@property="og:image"]/@content')
+        else ''
+    )
     return {
-        'title': title,
-        'description': description,
-        'site_name': site_name,
-        'image': image,
+        "title": title,
+        "description": description,
+        "site_name": site_name,
+        "image": image,
     }
