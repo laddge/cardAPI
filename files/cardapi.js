3c38c201952419d38f94b4c24157bf1c900782fc
Laddge <dev.laddge@gmail.com>
2021-06-03 10:27:48 +0900

    [fix] add files dir

---
+++ files/cardapi.js
@@ -0,0 +1,7 @@
+function CardAPI(className, template, override=true) {
+    for (const elem of document.getElementsByClassName(className)) {
+        if (elem.href) {
+            if (override) {
+
+    }
+}
