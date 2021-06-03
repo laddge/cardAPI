6bd5a8634eeb4c3418c0648d0e8c6c9030e6d9f5
Laddge <dev.laddge@gmail.com>
2021-06-03 13:20:38 +0900

    [feat] add js

--- files/cardapi.js
+++ files/cardapi.js
@@ -1,7 +1,28 @@
-function CardAPI(className, template, override=true) {
-    for (const elem of document.getElementsByClassName(className)) {
-        if (elem.href) {
-            if (override) {
-
+function CardAPI(className, template, override = true) {
+    'use strict';
+    this.load = function() {
+        for (const elem of document.getElementsByClassName(className)) {
+            if (elem.href) {
+                const api = 'https://cardapi.laddge.tk/?url=' + encodeURI(elem.href);
+                fetch(api)
+                    .then(data => {
+                        return data.json();
+                    })
+                    .then(json => {
+                        if (data != {}) {
+                            const rendered = template
+                                .replace(/{{ title }}/g, data.title)
+                                .replace(/{{ description }}/g, data.description)
+                                .replace(/{{ site_name }}/g, data.site_name)
+                                .replace(/{{ image }}/g, data.image);
+                            if (override) {
+                                elem.outerHTML = rendered;
+                            } else {
+                                elem.innerHTML = rendered;
+                            }
+                        }
+                    });
+            }
+        }
     }
 }
